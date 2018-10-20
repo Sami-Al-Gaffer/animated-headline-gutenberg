3ec64b86ef2e10dcc75ecc511e3254696aeab801
Sami <samialgaffer@gmail.com>
2018-10-20 09:40:35 +0600

    success

--- animated-headline-gutenberg.php
+++ animated-headline-gutenberg.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: sami Gutenberg
+ * Plugin Name: samigutenberg
  * Plugin URI: https://github.com/HardeepAsrani/hello-gutenberg/
  * Description: Gutenberg examples.
  * Author: Hardeep Asrani
@@ -20,45 +20,53 @@ defined('ABSPATH') || exit;
 
 //Check The Gutenberg
 
-if(function_exists('the_gutenbert_project')){
-	if(!function_exists('jmogutenberg_block')){
+// if(function_exists('the_gutenbert_project')){
+// 	if(!function_exists('jmogutenberg_block')){
 
-		function jmogutenberg_block(){
+		function samiGutenberg_block(){
 			//Register the block building script
 
-			wp_register_script( 
-				'jmogutenberg-myfirstblock-editor',  //name of the script
-				 plugins_url('blocks/myfirstblock/editor-script.js', __FILE__ ), //Url of scripts
-				 array( 'wp-blocks','wp-element' )  //required dependencies for gutenberg
-			 );
+			wp_register_script(
+			    'samigutenberg-myfirstblock-editor',
+			    plugins_url('blocks/myfirstblock/editor-script.js', __FILE__ ),
+			    array( 'wp-blocks', 'wp-element' )
+			);
+
+
+			// wp_register_script( 
+			// 	'jmogutenberg-myfirstblock-editor',  //name of the script
+			// 	 plugins_url('/blocks/myfirstblock/editor-script.js',__FILE__), //Url of scripts
+			// 	 array( 'wp-blocks','wp-element' )  //required dependencies for gutenberg
+			//  );
 
 				//Register global block css
 
 			wp_register_style( 
-				'jmogutenberg-myfirstblock',  //name of the script
-				 plugins_url('/blocks/myfirstblock/style.css',__FILE__), //Url of scripts
+				'samigutenberg-myfirstblock',  //name of the script
+				 plugins_url('blocks/myfirstblock/style.css',__FILE__), //Url of scripts
 				  array( 'wp-edit-blocks' ),  //required dependencies for gutenberg
-				  filemtime(plugin_dir_path(__FILE__) . '/blocks/myfirstblock/style.css')
+				  filemtime( plugin_dir_path(__FILE__) . 'blocks/myfirstblock/style.css')
 			 );
 
 			// //register editor only css
 
 			wp_register_style( 
-				'jmogutenberg-myfirstblock-editor',  //name of the style
-				 plugins_url('/blocks/myfirstblock/editor-style.css',__FILE__), //Url of scripts
+				'samigutenberg-myfirstblock-editor',  //name of the style
+				 plugins_url('blocks/myfirstblock/editor-style.css',__FILE__), //Url of scripts
 				  array( 'wp-edit-blocks' ),  //required dependencies for gutenberg
-				  filemtime(plugin_dir_path(__FILE__) . '/blocks/myfirstblock/editor-style.css')
+				  filemtime(plugin_dir_path(__FILE__) . 'blocks/myfirstblock/editor-style.css')
 			 );
 
 				//register block type
-			Register_block_type('jmogutenberg/myfirstblock',array(
-				'editor-script' 		=> 'jmogutenberg-myfirstblock-editor',
-				'editor-style'			=> 'jmogutenberg-myfirstblock-editor',
-				'style'					=> 'jmogutenberg-myfirstblock',
+			register_block_type('samigutenberg/myfirstblock', array(
+				'editor_script' => 'samigutenberg-myfirstblock-editor',
+				'editor_style'	=> 'samigutenberg-myfirstblock-editor',
+				'style'			=> 'samigutenberg-myfirstblock',
 			));
 		}
+		//add_action( 'enqueue_block_assets', 'jmogutenberg_block' );
 
-		add_action( 'init', 'jmogutenberg_block' );
-	}
+		add_action( 'init', 'samiGutenberg_block' );
+// 	}
 
-}
+// }
--- blocks/myfirstblock/editor-script.js
+++ blocks/myfirstblock/editor-script.js
@@ -1,84 +1,84 @@
-wp.blocks.registerBlockType('jmogutenberg/myfirstblock',{
-	title : 'My First Block',
-	icon : 'dashicons-welcome-add-page',
-	category: 'common',
-	attributes:{
-		content:{
-			type:'array',
-			source: 'children',
-			selector:'p',
-		},
-	},
-
-	edit: function(props){
-		return wp.element.createElement(wp.blocks.RichText,{
-			tagName: 'p',
-			className: props.className,
-			value: props.attributes.content,
-			onchange: function(newContent){
-				props.setAttributes({content: newContent});
-			}
-		});
-	},
-
-	save: function(props){
-		return wp.element.createElement('p',{
-			className: props.className,
-			//value: props.attributes.content
-		},props.attributes.content);
-		}
-		
-	
-});
-
-
-// var el = wp.element.createElement,
-//     registerBlockType = wp.blocks.registerBlockType,
-//     RichText = wp.editor.RichText;
-
-//    registerBlockType( 'jmogutenberg/myfirstblock', {
-//     title: 'My First Block',
-
-//     icon: 'welcome-add-page',
-
-//     category: 'layout',
-
-//     attributes: {
-//         content: {
-//             type:'array',
-//  			source: 'children',
+// wp.blocks.registerBlockType('samigutenberg/myfirstblock',{
+// 	title : 'My First Block',
+// 	icon : 'welcome-add-page',
+// 	category: 'common',
+// 	attributes:{
+// 		content:{
+// 			type:'array',
+// 			source: 'children',
 // 			selector:'p',
-//         }
-//     },
+// 		},
+// 	},
 
+// 	edit: function(props){
+// 		return wp.element.createElement(wp.blocks.RichText, {
+// 			tagName: 'p',
+// 			className: props.className,
+// 			value: props.attributes.content,
+// 			onchange: function( newContent ){
+// 				props.setAttributes({content: newContent});
+// 			}
+// 		});
+// 	},
 
-// 	edit: function( props ) {
-//         var content = props.attributes.content;
-
-//         function onChangeContent( newContent ) {
-//             props.setAttributes( { content: newContent } );
-//         }
-
-//         return el(
-//             RichText,
-//             {
-//                 tagName: 'p',
-//                 className: props.className,
-//                 onChange: onChangeContent,
-//                 value: content,
-//             }
-//         );
-//     },
-
-//         save: function( props ) {
-//         var content = props.attributes.content;
-
-//         return el( RichText.Content, {
-//             tagName: 'p',
-//             className: props.className,
-//             value: content
-//         } );
-//     },
+// 	save: function(props){
+// 		return wp.element.createElement('p',{
+// 			className: props.className,
+// 			//value: props.attributes.content
+// 		},props.attributes.content);
+// 		}
 		
 	
 // });
+
+
+var el = wp.element.createElement;
+    registerBlockType = wp.blocks.registerBlockType;
+    RichText = wp.editor.RichText;
+
+   registerBlockType( 'samigutenberg/myfirstblock', {
+    title: 'My First Block',
+
+    icon: 'welcome-add-page',
+
+    category: 'layout',
+
+    attributes: {
+        content: {
+            type:'array',
+ 			source: 'children',
+			selector:'p',
+        }
+    },
+
+
+	edit: function( props ) {
+        var content = props.attributes.content;
+
+        function onChangeContent( newContent ) {
+            props.setAttributes( { content: newContent } );
+        }
+
+        return el(
+            RichText,
+            {
+                tagName: 'p',
+                className: props.className,
+                onChange: onChangeContent,
+                value: content,
+            }
+        );
+    },
+
+        save: function( props ) {
+        var content = props.attributes.content;
+
+        return el( RichText.Content, {
+            tagName: 'p',
+            className: props.className,
+            value: content
+        } );
+    },
+		
+	
+});
